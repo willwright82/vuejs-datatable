19bc98744e64526a994605819bba6ad5351003bd
Ben Ledbury <dream.without.sleeping@gmail.com>
2019-03-14 18:53:11 +0000

    Code review updates

--- src/components/tables/VDataTable.vue
+++ src/components/tables/VDataTable.vue
@@ -5,36 +5,27 @@
         <th
           v-for="column in columns"
           :key="column.dataKey"
-          :class="column.align ? column.align + ' ' + 'aligned' : ''"
-          >
+          :class="[column.align, 'aligned']"
+        >
           {{ column.name }}
         </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="campaign in display_rows()" :key="campaign.code">
+      <tr v-for="campaign in displayRows" :key="campaign.code">
         <td
           v-for="column in columns"
           :key="column.dataKey"
-          :class="column.align ? column.align + ' ' + 'aligned' : ''"
-          >
-          {{
-          column.formatValue ?
-          formatValue(column, campaign[column.dataKey])
-          : campaign[column.dataKey]
-          }}
+          :class="[column.align, 'aligned']"
+        >
+          {{ formatValue(column, campaign[column.dataKey]) }}
         </td>
       </tr>
       <tr
-        v-for="empty_row in (perPage - display_rows().length)"
-        :key="empty_row"
+        v-for="emptyRow in (perPage - displayRows.length)"
+        :key="emptyRow"
       >
-        <td
-          v-for="column in columns"
-          :key="column.dataKey"
-          >
-          &nbsp;
-        </td>
+        <td>&nbsp;</td>
       </tr>
     </tbody>
     <tfoot>
@@ -43,26 +34,26 @@
           <div class="ui pagination menu">
             <a
               class="icon item"
-              :class="currentPage <= 0 ? 'disabled' : ''"
-              @click="currentPage > 0 ? currentPage -= 1 : ''"
+              :class="{ 'disabled': currentPage <= 0 }"
+              @click="currentPage = Math.max(currentPage - 1, 0)"
             >
-              <i class="left chevron icon"></i>
+              <i class="left chevron icon" />
             </a>
             <a
               v-for="(page, index) in pages"
               :key="index"
               class="item"
-              :class="index === currentPage ? 'active' : ''"
+              :class="{ 'active': index === currentPage }"
               @click="currentPage = index"
               >
               {{ index+1 }}
             </a>
             <a
               class="icon item"
-              :class="currentPage >= pages - 1 ? 'disabled' : ''"
-              @click="currentPage < pages - 1 ? currentPage += 1 : ''"
+              :class="{ 'disabled': currentPage >= pages - 1 }"
+              @click="currentPage = Math.min(pages - 1, currentPage + 1)"
             >
-              <i class="right chevron icon"></i>
+              <i class="right chevron icon" />
             </a>
           </div>
           &nbsp;
@@ -77,34 +68,43 @@
 <script>
 
 export default {
-  name: 'v-data-table',
+  name: 'VDataTable',
   props: {
-    columns: Array,
-    rows: Array,
-    perPage: Number,
+    columns: {
+      type: Array,
+      required: true,
+    },
+    rows: {
+      type: Array,
+      required: true,
+    },
+    perPage: {
+      type: Number,
+      default: 10,
+    },
   },
   data() {
     return {
       currentPage: 0,
-    }
+    };
   },
   methods: {
     formatValue(column, value) {
-      return column.formatValue(value)
-    },
-    display_rows() {
-      var start = (this.currentPage) * this.perPage
-      var end = start + this.perPage
-      return this.rows.slice(start, end)
-    },
-    change_page(page) {
-      this.currentPage = page
+      if (column.formatValue) {
+        return column.formatValue(value);
+      }
+      return value;
     },
   },
   computed: {
     pages() {
-      return Math.ceil(this.rows.length/this.perPage)
-    }
+      return Math.ceil(this.rows.length / this.perPage);
+    },
+    displayRows() {
+      var start = this.currentPage * this.perPage;
+      var end = start + this.perPage;
+      return this.rows.slice(start, end);
+    },
   },
 }
 </script>
